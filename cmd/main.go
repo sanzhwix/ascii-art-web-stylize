d7ecfb63c2112af301eedddb916d91057dd61f8c
sserzhan <dadlnpubg@gmail.com>
2025-07-13 20:37:11 +0500

    0

--- cmd/main.go
+++ cmd/main.go
@@ -1,108 +1,25 @@
 package main
 
 import (
-	"bytes"
+	"ascii-art/internal/handlers"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
-
-	print "ascii-art/art"
-	validators "ascii-art/validation"
 )
-
-var (
-	tpl *template.Template
-	mux *http.ServeMux
-)
-
-type Data struct {
-	Input  string
-	Output string
-	Full   bool
-	Banner string
-}
-
-func mainHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		RenderErrorPage(w, "Error 404 Page not found", http.StatusNotFound)
-		return
-	}
-	tpl.ExecuteTemplate(w, "index.html", nil)
-}
-
-func printHandleFunc(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodPost {
-		r.ParseForm()
-		font := r.FormValue("type")
-		text := r.FormValue("text")
-
-		if !validators.AsciiCharValidation(text) {
-			RenderErrorPage(w, "Error 404: Only strandard Ascii charecters are allowed!", http.StatusBadRequest)
-			return
-		}
-		if !validators.BannerValidity(font) {
-			RenderErrorPage(w, "Error 500 Internal server error "+font+" banner was changed!", http.StatusInternalServerError)
-			return
-		}
-		if len(text) > 100000 {
-			RenderErrorPage(w, "Request is too large!", http.StatusBadRequest)
-			return
-		}
-		// font == "" ||
-		if text == "" {
-			RenderErrorPage(w, "Error 400 Bad request", http.StatusBadRequest)
-			return
-		}
-		var buf bytes.Buffer
-		print.Processing(text, font, &buf)
-		res := Data{
-			Input:  text,
-			Output: buf.String(),
-			Full:   true,
-			Banner: font,
-		}
-		tpl.Execute(w, res)
-	} else {
-		RenderErrorPage(w, "Error 405 Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-}
-
-type ErrorType struct {
-	Msg string
-}
-
-func RenderErrorPage(w http.ResponseWriter, msg string, statusCode int) {
-	t, err := template.ParseFiles("templates/errorPage.html")
-	if err != nil {
-		fmt.Println("Error parsing error page")
-		return
-	}
-	w.WriteHeader(statusCode)
-	data := ErrorType{Msg: msg}
-	err = t.Execute(w, data)
-	if err != nil {
-		fmt.Println("Error with executing error page html")
-		return
-	}
-}
 
 func main() {
 	var err error
-
-	tpl, err = template.ParseFiles("templates/index.html")
+	handlers.Tpl, err = template.ParseGlob("templates/*.html")
 	if err != nil {
-		log.Fatal("Template parsing error:", err)
+		log.Fatal("Something went wrong with templates parsing!")
 	}
 
-	mux = http.NewServeMux()
-
+	mux := http.NewServeMux()
 	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+	mux.HandleFunc("/", handlers.MainHandler)
+	mux.HandleFunc("/print", handlers.PrintHandleFunc)
 
-	mux.HandleFunc("/", mainHandler)
-	mux.HandleFunc("/print", printHandleFunc)
-
-	fmt.Println("Server is running here: http://localhost:8080")
-	http.ListenAndServe(":8080", mux)
+	fmt.Println("Server running at http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", mux))
 }
---
+++ internal/handlers/handlers.go
@@ -0,0 +1,83 @@
+package handlers
+
+import (
+	"bytes"
+	"fmt"
+	"html/template"
+	"net/http"
+
+	print "ascii-art/art"
+	validators "ascii-art/validation"
+)
+
+var (
+	Tpl *template.Template
+	mux *http.ServeMux
+)
+
+type Data struct {
+	Input  string
+	Output string
+	Full   bool
+	Banner string
+}
+
+func MainHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		RenderErrorPage(w, "Error 404 Page not found", http.StatusNotFound)
+		return
+	}
+	Tpl.ExecuteTemplate(w, "index.html", nil)
+}
+
+func PrintHandleFunc(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		r.ParseForm()
+		font := r.FormValue("type")
+		text := r.FormValue("text")
+		maxInput := 100000
+
+		if !validators.AsciiCharValidation(text) {
+			RenderErrorPage(w, "Error 404: Only strandard ASCII charecters are allowed!", http.StatusBadRequest)
+			return
+		}
+		if !validators.BannerValidity(font) {
+			RenderErrorPage(w, "Error 500 Internal server error "+font+" banner was changed!", http.StatusInternalServerError)
+			return
+		}
+		if len(text) > maxInput {
+			RenderErrorPage(w, "Request is too large!", http.StatusBadRequest)
+			return
+		}
+		if text == "" {
+			RenderErrorPage(w, "Error 400 Bad request", http.StatusBadRequest)
+			return
+		}
+		var buf bytes.Buffer
+		print.Processing(text, font, &buf)
+		res := Data{
+			Input:  text,
+			Output: buf.String(),
+			Full:   true,
+			Banner: font,
+		}
+		Tpl.Execute(w, res)
+	} else {
+		RenderErrorPage(w, "Error 405 Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+}
+
+type ErrorType struct {
+	Msg string
+}
+
+func RenderErrorPage(w http.ResponseWriter, msg string, statusCode int) {
+	w.WriteHeader(statusCode)
+	data := ErrorType{Msg: msg}
+	err := Tpl.ExecuteTemplate(w, "errorPage.html", data)
+	if err != nil {
+		fmt.Println("Error with executing error page html")
+		return
+	}
+}
